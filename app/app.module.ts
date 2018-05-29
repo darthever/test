fcf326937b06fbf22cb193d1396bf07f8e944ad6
darthever <30493819+darthever@users.noreply.github.com>
2018-05-29 12:22:44 +0200

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,1 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+//import { CustomersModule } from './customers/customers.module';
+import { OrdersModule } from './orders/orders.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent,
+    HomeComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+   // CustomersModule,
+    OrdersModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
 
